3dd4f6aefcd85b440e465e702f7f9745819aacf3
Vladislav <nicochan00@mail.ru>
2022-07-16 01:14:59 +0300

    final project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+// burger-menu
+
+const mobileNavBtn = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+const body = document.body;
+
+mobileNavBtn.addEventListener('click', clickHandler);
+
+function clickHandler() {
+   mobileNavIcon.classList.toggle('active');
+   mobileNav.classList.toggle('active');
+   body.classList.toggle('no-scroll');
+   window.scrollTo(0, 0);
+}
